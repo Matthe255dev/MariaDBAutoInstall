6ccfec7240b66739087a7fc5b87f961e767531f8
Matthe255dev <116518249+Matthe255dev@users.noreply.github.com>
2023-06-23 22:48:21 +0200

    Update
    update start.sh and part1

--- mariadb-autoinstall-part1.sh
+++ mariadb-autoinstall-part1.sh
@@ -1,6 +1,3 @@
-apt update
-apt upgrade -y
-sleep 3
 apt install apache2 php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 -y
 sleep 3
 apt install mariadb-server mariadb-client -y
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 apt update
+apt upgrade -y
 sleep 2
 apt install zsh -y
 sleep 2
@@ -6,4 +7,4 @@ chmod +x mariadb-autoinstall-part1.sh
 chmod +x mariadb-autoinstall-part2.sh
 chmod +x install.sql
 sleep 1
-zsh mariadb-autoinstall-part1.sh
+./mariadb-autoinstall-part1.sh
