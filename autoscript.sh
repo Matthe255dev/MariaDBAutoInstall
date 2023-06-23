fd57396f97946d1d27cb1ad9dfe99655ba40f7ce
Matthe255dev <116518249+Matthe255dev@users.noreply.github.com>
2023-06-23 23:23:08 +0200

    add reboot to autoscript.sh

--- autoscript.sh
+++ autoscript.sh
@@ -22,7 +22,26 @@ Alias /phpmyadmin /usr/share/phpmyadmin
 </Directory>" > phpmyadmin.conf
 sleep 5
 a2enconf phpmyadmin
-systemctl reload apache2
+systemctl restart apache2
 mkdir /usr/share/phpmyadmin/temp
 chown -R www-data:www-data /usr/share/phpmyadmin/temp
 mysql -u root -p < mysql.sql
+echo "
+
+
+
+
+rebooting in 30 seconds"
+sleep 10
+echo "rebooting in 20 seconds"
+sleep 10
+echo "rebooting in 10 seconds"
+sleep 7
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+echo "rebooting now!"
+reboot
