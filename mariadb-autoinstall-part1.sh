b0eea6e3d9b567e92ccfbfe85d3767fef4f60ce4
Matthe255dev <116518249+Matthe255dev@users.noreply.github.com>
2023-06-23 22:36:43 +0200

    Update files
    update part1 to remove bugs

--- mariadb-autoinstall-part1.sh
+++ mariadb-autoinstall-part1.sh
@@ -1,6 +1,6 @@
-apt update && apt install apache2 -y
-sleep 1
-apt install php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 -y
-sleep 1
+apt update && apt upgrade -y
+sleep 5
+apt install apache2 php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 -y
+sleep 5
 apt install mariadb-server mariadb-client -y
 mariadb_secure_installation
