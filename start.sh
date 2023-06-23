995dc47993ef4b55346bf98e5036a31a2f51046e
Matthe255dev <116518249+Matthe255dev@users.noreply.github.com>
2023-06-23 22:51:20 +0200

    Debugging
    Added debug-lines

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
+echo "updating..."
 apt update
-apt upgrade -y
+#apt upgrade -y
 sleep 2
+echo "installing zsh"
 apt install zsh -y
 sleep 2
+echo "making scripts executable"
 chmod +x mariadb-autoinstall-part1.sh
 chmod +x mariadb-autoinstall-part2.sh
 chmod +x install.sql
 sleep 1
+echo "start part1"
 ./mariadb-autoinstall-part1.sh
