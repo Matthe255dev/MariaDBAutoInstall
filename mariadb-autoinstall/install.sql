86580387cffcfcee6b3312c85c577680a64907c4
Matthe255dev <116518249+Matthe255dev@users.noreply.github.com>
2023-06-23 22:26:27 +0200

    Delete mariadb-autoinstall directory

--- mariadb-autoinstall/install.sql
+++
@@ -1,4 +0,0 @@
-UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root' AND plugin = 'unix_socket';
-FLUSH PRIVILEGES;
-CREATE USER 'Admin'@'' IDENTIFIED BY 'autoinstall';
-GRANT ALL PRIVILEGES ON *.* TO 'Admin'@'' WITH GRANT OPTION;
--- mariadb-autoinstall/mariadb-autoinstall-part1.sh
+++
@@ -1,6 +0,0 @@
-apt update && apt install apache2 -y
-sleep 1
-apt install php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 -y
-sleep 1
-apt install mariadb-server mariadb-client -y
-mariadb_secure_installation
