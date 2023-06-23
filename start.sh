3d2f96a52adeb0fb7d3a3060f91d8addec958159
Matthe255dev <116518249+Matthe255dev@users.noreply.github.com>
2023-06-23 23:00:51 +0200

    Update
    remove part1 and integrate commands into start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,8 @@
-echo "updating..."
 apt update
-#apt upgrade -y
 sleep 2
 echo "installing zsh"
-apt install zsh -y
-sleep 2
-echo "making scripts executable"
-chmod +x mariadb-autoinstall-part1.sh
+apt install mariadb-server mariadb-client zsh apache2 php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7-4-bz2 libapache2-mod-php7.4 -y
 chmod +x mariadb-autoinstall-part2.sh
 chmod +x install.sql
 sleep 1
-echo "start part1"
-./mariadb-autoinstall-part1.sh
+mariadb_secure_install
